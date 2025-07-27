4c551d964cbaf4dc8361cea1b3db753f2120f885
Geetha-S-03 <168105598+Geetha-S-03@users.noreply.github.com>
2025-07-27 21:59:55 +0530

    Create ImplementBasicMathOperations.kt

---
+++ ImplementBasicMathOperations.kt
@@ -0,0 +1,24 @@
+/*7. Implement basic math operations
+In this exercise, you write a program that performs basic math operations and prints the output.
+
+Step 1
+This main() function contains one compile error:
+
+
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    
+    println("$firstNumber + $secondNumber = $result")
+}
+Can you fix the error so that the program prints this output?
+
+10 + 5 = 15*/
+
+
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val result=firstNumber+secondNumber
+    println("$firstNumber + $secondNumber = $result")
+}
